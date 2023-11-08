62eb8c938ea201c8e24bf9cc246ef2b6961ada96
Gyanapriya Pradhan (Zed) <gyana.cse7@gmail.com>
2023-11-08 16:21:33 +0530

    updated
    schedule this script to run at regular intervals using cron jobs. For example, to check the website every 30 minutes, you can add the following line to your crontab:
    */30 * * * * /path/to/web_page_monitoring_script.sh
    Make sure to replace /path/to/web_page_monitoring_script.sh with the actual path to your monitoring script.
    
    
    This script will send you an email alert if the monitored website is inaccessible, helping you keep track of its status

--- web-page-monitor.bash
+++ web-page-monitor.bash
@@ -2,12 +2,16 @@
 
 # Web Page Monitoring Script
 
-url="https://example.com"
-response=$(curl -Is $url | head -n 1 | cut -d ' ' -f2)
+url="https://example.com"  # Replace with the URL of the web page you want to monitor
+email="your@email.com"    # Replace with your email address for alerts
+
+response=$(curl -Is "$url" | head -n 1 | cut -d ' ' -f2)
 
 if [ "$response" == "200" ]; then
-    echo "The website is accessible."
+    echo "The website $url is accessible."
 else
-    echo "The website is down. HTTP Status Code: $response"
+    echo "The website $url is down. HTTP Status Code: $response"
+    echo "Sending email alert..."
+    echo "The website $url is down. HTTP Status Code: $response" | mail -s "Website Status Alert" "$email"
 fi
     
