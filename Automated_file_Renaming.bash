7c03bf3c844cda55669f1b64a31562592dfa3481
Gyanapriya Pradhan (Zed) <gyana.cse7@gmail.com>
2023-12-26 08:35:00 +0530

    Initial commit

---
+++ Automated_file_Renaming.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Automated File Renaming
+
+echo "Enter the directory containing files to rename:"
+read directory
+
+echo "Enter the naming pattern (e.g., 'image_'):"
+read pattern
+
+counter=1
+
+for file in "$directory"/*; do
+    extension="${file##*.}"
+    new_name="$directory/$pattern$counter.$extension"
+    mv "$file" "$new_name"
+    ((counter++))
+done
+
+echo "File renaming completed."
